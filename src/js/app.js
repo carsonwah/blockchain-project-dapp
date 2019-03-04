2d3f5eaaabde81560e8b1b108c8f8eadd95fd113
barryckc <barryckc@gmail.com>
2019-03-05 00:27:57 +0800

    Update gitignore and connect eth

--- src/js/app.js
+++ src/js/app.js
@@ -3,60 +3,51 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // if no web3 instance provided add a new one from Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
+    $.getJSON("Quiz.json", function(quiz) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(quiz);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+      return;
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  loadAccountInfo: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  loadQuiz: function() {
+
   },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+  answerQuiz: function() {
 
-    var petId = parseInt($(event.target).data('id'));
+  },
 
-    /*
-     * Replace me...
-     */
+  releaseAnswer: function() {
+
   }
 
 };
