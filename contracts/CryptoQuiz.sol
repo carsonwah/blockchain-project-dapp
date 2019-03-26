482d15688d81b81f359e8d7b27003885e03ea0e1
carsonwah <carsonwah@gmail.com>
2019-03-26 20:04:22 +0800

    Update smart contract

--- contracts/CryptoQuiz.sol
+++ contracts/CryptoQuiz.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.6;
 
 contract CryptoQuiz {
 
@@ -8,15 +8,23 @@ contract CryptoQuiz {
     }
 
     struct Question {
-        bytes32 questionId;  // To prevent problems from partial order of the array
-        string question;  // Question itself
-        Answer[] answers;  // Received answers
+        // Question details
+        bytes32 questionId;  // For checking. (To prevent problems from partial order of the array)
+        string questionStr;  // Question itself
+
+        // Received answers
+        uint answersCount;
+        mapping(uint => Answer) answers;
         mapping(address => bool) answeredStudents;
-        bytes32 publicKey;  // 256-bit ECDSA key, For encrypting answer
+
+        // Secrets of answer
+        bytes32 publicKey;  // 256-bit ECIES key (secp256k1), For encrypting answer
         // uint32 deadline;  // Deadline. (Should be large enough because it is compared with block.timestamp)
-        bool revealed;  // Answer revealed or not
         bytes32 privateKey;  // Not exist until revealed
         bytes32 trueAnswer;  // Not exist until revealed
+
+        // Status of the question
+        bool revealed;  // Answer revealed or not
         bool pointsDistributed;  // Whether points have been distributed to students or not
     }
 
@@ -33,12 +41,14 @@ contract CryptoQuiz {
     mapping(address => Student) public students;
 
     // Questions list
-    Question[] public questions;
+    uint questionsCount;
+    mapping(uint => Question) public questions;
 
     event QuestionRevealed(uint questionIndex, string answer);
 
     constructor() public {
         professor = msg.sender;
+        questionsCount = 0;
     }
 
     modifier onlyProfessor {
@@ -55,18 +65,19 @@ contract CryptoQuiz {
         students[_studentAddress] = Student(true, 0);
     }
 
-    function postQuestion(bytes32 _questionId, string memory _question, bytes32 _publicKey) public onlyProfessor {
-        Question memory question = Question(
-            _questionId,
-            _question,
-            new Answer[](0),
-            _publicKey,
-            false,
-            "",
-            "",
-            false
-            );
-        questions.push(question);
+    function postQuestion(bytes32 _questionId, string memory _questionStr, bytes32 _publicKey) public onlyProfessor {
+        Question memory question = Question({
+            questionId: _questionId,
+            questionStr: _questionStr,
+            answersCount: 0,
+            publicKey: _publicKey,
+            revealed: false,
+            privateKey: "",
+            trueAnswer: "",
+            pointsDistributed: false
+        });
+        questions[questionsCount] = question;
+        questionsCount++;
     }
 
     /**
@@ -79,7 +90,8 @@ contract CryptoQuiz {
         require(!question.answeredStudents[msg.sender], "This student already answered this question.");
 
         // Add answer to question
-        question.answers.push(Answer(msg.sender, _hashedAnswer));
+        question.answers[question.answersCount] = Answer(msg.sender, _hashedAnswer);
+        question.answersCount++;
 
         // Indicate already answered
         question.answeredStudents[msg.sender] = true;
@@ -97,17 +109,18 @@ contract CryptoQuiz {
     }
 
     /**
-        decryptedAnswers: professor should decrypt answers offline and submit them here
+        decryptedAnswers: professor should decrypt answers from students offline, and submit them here
+        (Because it's too expensive to be computed on-chain)
      */
     function distributePoints(bytes32 _questionId, uint _questionIndex, bytes32[] memory decryptedAnswers) public onlyProfessor {
         Question storage question = questions[_questionIndex];
         require(question.questionId == _questionId, "Different question.");
         require(question.revealed, "Answer not yet revealed.");
         require(!question.pointsDistributed, "Points already distributed.");
-        require(decryptedAnswers.length == question.answers.length, "Invalid decryptedAnswers.");
+        require(decryptedAnswers.length == question.answersCount, "Invalid decryptedAnswers.");
 
         // Send reward to students
-        for (uint i = 0; i<question.answers.length; i++) {
+        for (uint i = 0; i<question.answersCount; i++) {
             if (decryptedAnswers[i] == question.trueAnswer) {
                 // Answered correctly
                 address studentAddress = question.answers[i].byStudent;
