903638d19020991d2b0dda7a8b0a3abaafb2506a
barryckc <barryckc@gmail.com>
2019-04-01 13:06:55 +0800

    Add professor page ui - student panel and question panel

--- src/js/app_professor.js
+++ src/js/app_professor.js
@@ -42,7 +42,7 @@ App = {
       web3.eth.getCoinbase(function(err, account) {
         if (err === null) {
           App.account = account;
-          $("#accountAddress").html("Your Account: " + account);
+          $("#account-address").html("Hi, Professor. Your Account is: " + account);
         }
       });
   
@@ -51,18 +51,73 @@ App = {
         CryptoQuizInstance = instance;
         return CryptoQuizInstance.professor();
       }).then(function(professor) {
-        
         if (professor != App.account) {
             alert("You are not professor!");
             window.location.href = 'index.html';
+        }
+        return CryptoQuizInstance.questionsCount();
+      }).then(function(questionsCount) {
+        
+        for(var i = 0; i <= questionsCount; i++) {
+          CryptoQuizInstance.questions(i).then(function(question) {
+            var questionList = $("#questions-list");
+            var questionId = question[0];
+            var questionStr = question[1];
+            var publicKey = question[2];
+            console.log("publicKey: " + publicKey);
+            var questionTemplate = '<tr><td style="overflow: scroll;">'+questionId+'</td><td style="overflow-wrap: break-word;">'+questionStr+'</td><td><button class="btn btn-success" type="button" onclick="App.revealAnswer()">Reveal Answer</button></td></tr>';
+            questionList.append(questionTemplate);
+          });
         }
         loader.hide();
         content.show();
       }).catch(function(error) {
         console.warn(error);
       });
+    },
+
+    addStudent: function() {
+      var studentAddr = $("#student-addr");
+      var studentList = $("#student-list");
+      // Add student
+      App.contracts.CryptoQuiz.deployed().then(function(instance) {
+        CryptoQuizInstance = instance;
+        return CryptoQuizInstance.addStudent(studentAddr.val());
+      }).then(function(result) {
+        var studentTemplate = '<li class="list-group-item">'+studentAddr.val()+'</li>';
+        studentList.append(studentTemplate);
+        studentAddr.val('');
+        console.log("New Student Added");
+      }).catch(function(error) {
+        console.warn("addStudent(): " + error);
+      });
+    },
+
+    postQuestion: function() {
+      var newQuestion = $("#new-question");
+      var questionList = $("#questions-list");
+
+      var questionId = "1";
+      var questionStr = newQuestion.val();
+      var publicKey = "abcde";
+
+      // Post new question
+      App.contracts.CryptoQuiz.deployed().then(function(instance) {
+        CryptoQuizInstance = instance;
+        return CryptoQuizInstance.postQuestion(questionId, questionStr, publicKey);
+      }).then(function(result) {
+        var questionTemplate = '<tr><td style="overflow: scroll;">'+questionId+'</td><td style="overflow-wrap: break-word;">'+questionStr+'</td><td><button class="btn btn-success" type="button" onclick="App.revealAnswer()">Reveal Answer</button></td></tr>';
+        questionList.append(questionTemplate);
+        newQuestion.val('');
+        console.log("New Question Posted");
+      }).catch(function(error) {
+        console.warn("postQuestion(): " + error);
+      });
+    },
+
+    revealAnswer: function() {
+      console.log("revealAnswer()");
     }
-  
   };
   
   $(function() {
