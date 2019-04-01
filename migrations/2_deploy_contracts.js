bd718165d2dae5e3f2e781fda10e9bd38d5d1f02
carsonwah <carsonwah@gmail.com>
2019-04-02 03:44:54 +0800

    Send ether to CryptoQuiz when deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,48 +5,21 @@ var CryptoQuizTokenSale = artifacts.require("./CryptoQuizTokenSale.sol");
 module.exports = async function(deployer, network, accounts) {
   const tokensSupply = 1000000;
   const tokenForSale = 500000;
-  // const tokenPrice = 1000000000000000;
+  const tokenForQuiz = 250000;
   const tokenPrice = 1e16.toString();  // 0.01 ether
-  // const tokenPrice = 1;  // 0.01 eth
   const deployAccountAddress = accounts[0];
-
-  // deployer.deploy(CryptoQuizToken, tokensSupply).then(function() {
-  //   console.log('CryptoQuizToken.address: ', CryptoQuizToken.address);
-
-  //   return deployer.deploy(CryptoQuiz, CryptoQuizToken.address)
-  //     .then(function() {
-  //       console.log('CryptoQuiz.address: ', CryptoQuiz.address);
-
-  //       // Token price is 0.001 Ether
-  //       var tokenPrice = 1000000000000000;
-  //       return deployer.deploy(CryptoQuizTokenSale, CryptoQuizToken.address, tokenPrice)
-  //         .then(function() {
-  //           console.log('CryptoQuizTokenSale.address: ', CryptoQuizTokenSale.address);
-
-  //           // Transfer initial token supply
-  //           CryptoQuizToken.deployed()
-  //             .then(function(instance) {
-  //               instance.transfer(CryptoQuizTokenSale.address, tokenForSale, { from: deployAccountAddress });
-  //             });
-  //         });
-  //     });
-
-  // });
 
   await deployer.deploy(CryptoQuizToken, tokensSupply);
   await deployer.deploy(CryptoQuiz, CryptoQuizToken.address);
   await deployer.deploy(CryptoQuizTokenSale, CryptoQuizToken.address, tokenPrice)
-  .then(() => {
-  CryptoQuizToken.deployed()
-  .then(function(instance) {
-    console.log(`Sending ${tokenForSale} from ${deployAccountAddress}(owner) to ${CryptoQuizTokenSale.address}(tokenSale)`);
-    instance.transfer(CryptoQuizTokenSale.address, tokenForSale, { from: deployAccountAddress })
-      .then((result) => {
-        console.log('Success', result);
-      })
-      .catch(err => {
-        console.log('Failed', err);
-      })
-  });
-  });
+  const tokenContract = await CryptoQuizToken.deployed()
+  console.log(`Sending ${tokenForSale} from ${deployAccountAddress}(owner) to ${CryptoQuizTokenSale.address}(tokenSale)`);
+  const result1 = await tokenContract.transfer(CryptoQuizTokenSale.address, tokenForSale, { from: deployAccountAddress });
+  console.log('SUCCESS');
+  // console.log(result1);
+  console.log(`Sending ${tokenForQuiz} from ${deployAccountAddress}(owner) to ${CryptoQuiz.address}(CryptoQuiz)`);
+  const result2 = await tokenContract.transfer(CryptoQuiz.address, tokenForQuiz, { from: deployAccountAddress });
+  console.log('SUCCESS');
+  // console.log(result2);
+  console.log('Finished deployment.')
 };
